74dd409e28aede9c3396904b0615a21d76e01ef6
Tobias Lobitz <hallo@tobiaslobitz.de>
2024-09-24 10:22:41 +0200

    wip(seo): Add basic seo tags

--- app.vue
+++ app.vue
@@ -7,7 +7,22 @@ const email = ref('')
 const success = ref(false)
 const errors = ref<{ code: string, message: string, path: string [], validation: string }[]>([])
 
-useHead({title: 'Nuxt Hub Landing - Join the waitlist'})
+useHead({
+  bodyAttrs: {
+    class: 'bg-zinc-50 dark:bg-black'
+  }
+})
+
+useSeoMeta({
+  title: 'Validate your idea - setup your landing page in minutes | NuxtHubLanding',
+  applicationName: 'NuxtHubLanding',
+  description: 'Validate your idea quickly with the NuxtHub Boilerplate – a landing page template that lets you capture interest and sign up users in minutes. Launch your product in no time and start gathering feedback from real customers.',
+  ogType: 'website',
+  ogTitle: 'Validate your idea - setup your landing page in minutes | NuxtHubLanding',
+  ogDescription: 'Validate your idea quickly with the NuxtHub Boilerplate – a landing page template that lets you capture interest and sign up users in minutes. Launch your product in no time and start gathering feedback from real customers.',
+  twitterCard: 'summary_large_image',
+  twitterCreator: '@lowbits_',
+})
 
 const joinWaitlist = async () => {
   errors.value = []
@@ -37,20 +52,12 @@ const joinWaitlist = async () => {
   }
 }
 
-useHead({
-  bodyAttrs: {
-    class: 'bg-zinc-50 dark:bg-black'
-  }
-})
-
 const currentYear = computed(() => new Date().getFullYear())
 </script>
 <template>
   <div>
     <div class="mt-20 max-w-6xl mx-auto">
-
       <div class="flex justify-between">
-
 
         <div class="max-w-lg">
         <span class="px-1.5 py-1 text-xs bg-[#D65320] bg-opacity-80 text-gray-300">
